b7d58375a38b58c3b8f0b108de64d1979aa0332b
Hong-Phat Ly <h.ly@iasonconsulting.com>
2020-10-30 15:46:12 +0100

    Projektdateien hinzufügen.

---
+++ ComRest/ApiResponse.vb
@@ -0,0 +1,30 @@
+﻿Public MustInherit Class ApiResponse
+
+    Sub New()
+
+    End Sub
+
+    Sub New(IsSuccess As Boolean, Exception As Exception)
+        Me.IsSuccess = IsSuccess
+        Me.Exception = Exception
+    End Sub
+
+    Property IsSuccess As Boolean
+    Property Exception As Exception
+End Class
+
+Public Class ApiResponse(Of T)
+    Inherits ApiResponse
+
+    Sub New()
+
+    End Sub
+
+    Sub New(IsSuccess As Boolean, Result As T, Exception As Exception)
+        Me.IsSuccess = IsSuccess
+        Me.Result = Result
+        Me.Exception = Exception
+    End Sub
+
+    Property Result As T
+End Class
---
+++ ComRest/RestService.vb
@@ -0,0 +1,130 @@
+Imports System.Net.Http
+Imports System.Text
+Imports Newtonsoft.Json
+
+''' <summary>
+''' Represents the common rest service for the specific HTTP Methods: GET, POST, PUT and DELETE.
+''' </summary>
+Public Class RestService
+
+    ''' <summary>
+    ''' Gets data from a specific request URI asynchronously.
+    ''' </summary>
+    ''' <typeparam name="TReturn">object</typeparam>
+    ''' <param name="RequestUri">URI string</param>
+    ''' <returns>Returns a specific api response.</returns>
+    Public Shared Async Function GetAsync(Of TReturn)(RequestUri As String) As Task(Of ApiResponse(Of TReturn))
+        Using HttpClient As New HttpClient
+            Try
+                Dim JsonResult As String = Await HttpClient.GetStringAsync(RequestUri)
+
+                Return JsonConvert.DeserializeObject(Of ApiResponse(Of TReturn))(JsonResult)
+            Catch ex As Exception
+                Return New ApiResponse(Of TReturn)(False, Nothing, ex)
+            End Try
+        End Using
+    End Function
+
+    ''' <summary>
+    ''' Posts a specific content object to a specific request asynchronously.
+    ''' </summary>
+    ''' <typeparam name="TReturn">object</typeparam>
+    ''' <param name="RequestUri">URI string</param>
+    ''' <param name="Content">Content string</param>
+    ''' <returns>Returns a specific api response.</returns>
+    Public Shared Async Function PostAsync(Of TReturn)(RequestUri As String, Content As Object) As Task(Of ApiResponse(Of TReturn))
+        Using HttpClient As New HttpClient
+            Try
+                Dim SerializedObject As String = JsonConvert.SerializeObject(Content)
+
+                Dim HttpContent As New StringContent(SerializedObject, Encoding.UTF8, "application/json")
+
+                Return Await PostAsync(Of TReturn)(RequestUri, HttpContent)
+            Catch ex As Exception
+                Return New ApiResponse(Of TReturn)(False, Nothing, ex)
+            End Try
+        End Using
+    End Function
+
+    ''' <summary>
+    ''' Posts a specific HTTP Content to a specific request asynchronously.
+    ''' </summary>
+    ''' <typeparam name="TReturn">object</typeparam>
+    ''' <param name="RequestUri">URI string</param>
+    ''' <param name="Content">HTTP Content</param>
+    ''' <returns>Returns a specific api response.</returns>
+    Public Shared Async Function PostAsync(Of TReturn)(RequestUri As String, Content As HttpContent) As Task(Of ApiResponse(Of TReturn))
+        Using HttpClient As New HttpClient
+            Try
+                Dim Response As HttpResponseMessage = Await HttpClient.PostAsync(RequestUri, Content)
+                Dim JsonResult As String = Await Response.Content.ReadAsStringAsync()
+
+                Return JsonConvert.DeserializeObject(Of ApiResponse(Of TReturn))(JsonResult)
+            Catch ex As Exception
+                Return New ApiResponse(Of TReturn)(False, Nothing, ex)
+            End Try
+        End Using
+    End Function
+
+    ''' <summary>
+    ''' Puts a specific content object to a specific request asynchronously. 
+    ''' </summary>
+    ''' <typeparam name="TReturn">object</typeparam>
+    ''' <param name="RequestUri">URI string</param>
+    ''' <param name="Content">Content string</param>
+    ''' <returns>Returns a specific api response.</returns>
+    Public Shared Async Function PutAsync(Of TReturn)(RequestUri As String, Content As Object) As Task(Of ApiResponse(Of TReturn))
+        Using HttpClient As New HttpClient
+            Try
+                Dim SerializedObject As String = JsonConvert.SerializeObject(Content)
+
+                Dim HttpContent As New StringContent(SerializedObject, Encoding.UTF8, "application/json")
+
+                Return Await PutAsync(Of TReturn)(RequestUri, HttpContent)
+            Catch ex As Exception
+                Return New ApiResponse(Of TReturn)(False, Nothing, ex)
+            End Try
+        End Using
+    End Function
+
+    ''' <summary>
+    ''' Puts a specific HTTP content to a specific request asynchronously.
+    ''' </summary>
+    ''' <typeparam name="TReturn">object</typeparam>
+    ''' <param name="RequestUri">URI string</param>
+    ''' <param name="Content">Http content</param>
+    ''' <returns>Returns a specific api response.</returns>
+    Public Shared Async Function PutAsync(Of TReturn)(RequestUri As String, Content As HttpContent) As Task(Of ApiResponse(Of TReturn))
+        Using HttpClient As New HttpClient
+            Try
+                Dim Response As HttpResponseMessage = Await HttpClient.PutAsync(RequestUri, Content)
+                Dim JsonResult As String = Await Response.Content.ReadAsStringAsync()
+
+                Return JsonConvert.DeserializeObject(Of ApiResponse(Of TReturn))(JsonResult)
+            Catch ex As Exception
+                Return New ApiResponse(Of TReturn)(False, Nothing, ex)
+            End Try
+        End Using
+    End Function
+
+    ''' <summary>
+    ''' Deletes data from a specific request URI.
+    ''' </summary>
+    ''' <typeparam name="TReturn">object</typeparam>
+    ''' <param name="RequestUri">URI string</param>
+    ''' <returns>Returns a specific api response.</returns>
+    Public Shared Async Function DeleteAsync(Of TReturn)(RequestUri As String) As Task(Of ApiResponse(Of TReturn))
+        Using HttpClient As New HttpClient
+            Try
+                Dim Response As HttpResponseMessage = Await HttpClient.DeleteAsync(RequestUri)
+                Dim JsonResult As String = Await Response.Content.ReadAsStringAsync()
+
+                Return JsonConvert.DeserializeObject(Of ApiResponse(Of TReturn))(JsonResult)
+            Catch ex As Exception
+                Return New ApiResponse(Of TReturn)(False, Nothing, ex)
+            End Try
+        End Using
+    End Function
+
+End Class
+
